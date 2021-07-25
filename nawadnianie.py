4a7551fe6eed333ec9cb3fb49117f71c729f9fbb
grzymi <przemyslaw.grzymislawski@gmail.com>
2021-07-25 10:47:22 +0200

    Relay switch work

--- nawadnianie.py
+++ nawadnianie.py
@@ -10,3 +10,15 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.route('/status')
+def status():
+    relay1 = requests.post(url = 'http://192.168.0.169/cm?cmnd=Power1%20Status')
+    if '"ON"' in relay1.text:
+        relay1 = requests.post(url = 'http://192.168.0.169/cm?cmnd=Power1%20Off')
+        state = f'Switch OFF'
+    elif '"OFF"' in relay1.text:
+        relay1 = requests.post(url = 'http://192.168.0.169/cm?cmnd=Power1%20On')
+        state = f'Switch ON'
+    return render_template('status.html', state=state)
+    #return f'<h1> {status} </h1>'
