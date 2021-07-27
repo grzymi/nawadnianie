c68d38872b573ff67d1b93e10e37563242a41c0d
grzymi <przemyslaw.grzymislawski@gmail.com>
2021-07-27 22:33:56 +0200

    Dodanie nowych widokow

--- nawadnianie.py
+++ nawadnianie.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 import requests
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 
 @app.route('/')
 def index():
@@ -22,3 +25,10 @@ def status():
         state = f'Switch ON'
     return render_template('status.html', state=state)
     
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
