4c39e2904408a7eb71101906539c1495bc95a724
grzymi <przemyslaw.grzymislawski@gmail.com>
2021-07-27 22:49:42 +0200

    Dodano ikone

--- nawadnianie.py
+++ nawadnianie.py
@@ -8,7 +8,7 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @app.route('/user/<name>')
 def user(name):
