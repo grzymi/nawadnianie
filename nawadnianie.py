13a6857d3f8a2fc462390b06374b57addbf5eab9
grzymi <przemyslaw.grzymislawski@gmail.com>
2021-08-01 18:43:47 +0200

    Dodanie daty i godziny

--- nawadnianie.py
+++ nawadnianie.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 import requests
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('base.html', current_time = datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name, current_time = datetime.utcnow())
 
 @app.route('/status')
 def status():
