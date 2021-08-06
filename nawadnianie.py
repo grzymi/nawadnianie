8c7d20f830a6417d1499f4c93f7e5c46862bf3c8
grzymi <przemyslaw.grzymislawski@gmail.com>
2021-08-06 13:52:04 +0200

    Nowe elementy formularzy

--- nawadnianie.py
+++ nawadnianie.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -19,9 +19,26 @@ class NameForm(FlaskForm):
 
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('base.html', current_time = datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', current_time = datetime.utcnow(), form=form, name=session.get('name'))
+
+
+'''
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', current_time = datetime.utcnow(), form=form, name=name)
+'''
+
 
 @app.route('/user/<name>')
 def user(name):
